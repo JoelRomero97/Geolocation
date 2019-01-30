9a9bb516c90486dda22b5bad72ef504cd0ebf5d8
Joel Romero <joelromero@localhost.localdomain>
2019-01-29 20:48:30 -0600

    Exact location fixed. FitBounds to see all markers

--- src/app/main/map/map.component.ts
+++ src/app/main/map/map.component.ts
@@ -1,6 +1,9 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material';
 import {DistributorInterface, MarkerInterface} from '../../util/data.interface';
+import { LatLngBounds} from '@agm/core/services/google-maps-types';
+
+declare var google: any;
 
 @Component({
   selector: 'app-map',
@@ -8,10 +11,11 @@ import {DistributorInterface, MarkerInterface} from '../../util/data.interface';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  distributors: Array<DistributorInterface> = [];
-  markers: Array<MarkerInterface> = [];
   latitude: number;
   longitude: number;
+  markers: Array<MarkerInterface> = [];
+  distributors: Array<DistributorInterface> = [];
+  bounds: LatLngBounds = new google.maps.LatLngBounds();
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: {
     distributors: Array<DistributorInterface>,
@@ -22,10 +26,14 @@ export class MapComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.latitude = this.data.latitude;
-    this.longitude = this.data.longitude;
+    this.latitude = this.data.latitude + 0.012644400000002776;
+    this.longitude = this.data.longitude + 0.05518370000000061;
     this.markers = this.data.markers;
     this.distributors = this.data.distributors;
+    this.bounds.extend(new google.maps.LatLng(this.latitude, this.longitude));
+    this.markers.forEach((marker: MarkerInterface) => {
+      this.bounds.extend(new google.maps.LatLng(marker.latitude, marker.longitude));
+    });
   }
 
 }
